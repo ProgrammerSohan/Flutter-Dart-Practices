174739c4b7e531b48fe8e3b9f6e1f37fdcae389b
ProgrammerSohan <sohanofficial2022@gmail.com>
2025-12-25 03:00:34 +0600

    09.1-widgets-listview

--- lib/main.dart
+++ lib/main.dart
@@ -15,41 +15,30 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
        
-         body:  Column(
-         // mainAxisAlignment: MainAxisAlignment.center,
-         // mainAxisAlignment: MainAxisAlignment.end,
-         //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-         //mainAxisAlignment: MainAxisAlignment.spaceAround,
-        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //crossAxisAlignment: CrossAxisAlignment.center,
+         body:  ListView(
+        
             children:[
                 //1st box
-                Expanded(
-                  child: Container(
-                    //height: 200,
+               Container(
+                    height: 350,
                    // width: 200,
                     color: Colors.deepPurple,
                   
                   ),
-                ),
+                
               //2nd box
-                Expanded(
-                  flex:3,
-                  child: Container(
-                    //height: 200,
-                    //width: 200,
+                 Container(
+                    height: 350,
+                   // width: 200,
                     color: Colors.deepPurple[400],
                   
                   ),
-                ),
                 //3rd box
-                  Expanded(
-                    child: Container(
-                   // height: 200,
+                    Container(
+                    height: 350,
                    // width: 200,
                     color: Colors.deepPurple[200],
-                    
-                                    ),
+                  
                   ),
 
 
